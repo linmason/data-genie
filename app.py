a73874b94375414f4e9d401c122967b5cba85cc8
Josh Kennedy <kennedyjosh99@gmail.com>
2022-03-02 17:01:17 -0600

    front-end works, but does not transfer file

---
+++ app.py
@@ -0,0 +1,44 @@
+from cgitb import html
+from crypt import methods
+import re
+from webbrowser import get
+from flask import Flask, current_app, render_template, request, redirect, send_from_directory, url_for, flash, send_file
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['DOWNLOAD_FOLDER'] = 'downloads'
+
+download_file_name = "data_genie_output.csv"
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+    if request.method == 'POST':
+        uploaded_file = request.files['uploadname']
+        secure_file = secure_filename(uploaded_file.filename)
+        if secure_file != '':
+            uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'],secure_file))
+        return redirect(url_for('result'))
+    return render_template('result.html')
+
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+@app.route('/result/<path:filename>', methods=['GET','POST'])
+def download(filename):
+    print(filename)
+    downloads = os.path.join(current_app.root_path, app.config['DOWNLOAD_FOLDER'])
+    return send_from_directory(downloads, filename, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run()
