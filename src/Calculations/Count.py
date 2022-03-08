c7442553910692a014d8513d5ba158dec80821d7
Mason Lin <Mason@Masons-MacBook-Pro.local>
2022-03-08 13:57:43 -0600

    doesn'

--- src/Calculations/Count.py
+++ src/Calculations/Count.py
@@ -6,8 +6,11 @@ class Count(Calculation.Calculation):
         super().__init__("Count", vertical=True, horizontal=True)
 
     def do(self, data: [list]) -> list:
-        result = []
+        filtered_data = []
         for rc in data:
+            filtered_data.append(list(filter(lambda x: x and (type(x) in [int, float] or not x.isspace()), rc)))
+        result = []
+        for rc in filtered_data:
             result.append(len(rc))
         return result
 
